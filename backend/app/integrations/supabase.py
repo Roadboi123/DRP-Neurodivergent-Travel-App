86693cd826c77d7b67c426544acff8b5104f8549
Siva <siva.thalluri@gmail.com>
2026-05-28 17:45:54 +0100

    refactor: tidied up and cleaned up print statments

--- backend/app/integrations/supabase.py
+++ backend/app/integrations/supabase.py
@@ -1,9 +1,5 @@
 from supabase import create_client
 import os
-print("banana")
-print({k:v for k,v in os.environ.items()})
-print("SUPABASE_URL =", os.getenv("SUPABASE_URL"))
-print("SUPABASE_KEY exists =", bool(os.getenv("SUPABASE_KEY")))
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
